ff000601b75209d750b0b9f77f2a48b141bcff80
joyalaji <joyalmampillil4@gmail.com>
2026-03-17 16:19:46 -0400

    Update run-tests.sh

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -27,7 +27,7 @@ if ! grep -q "March 17, 2026" myapp/index.html; then
   exit 1
 fi
 
-if ! grep -q "Welcome to my site" myapp/index.html; then
+if ! grep -q "Welcome to my app" myapp/index.html; then
    echo "ERROR: WELCOME QOUTE IS MISSING." >&2
    exit 1 
 fi
