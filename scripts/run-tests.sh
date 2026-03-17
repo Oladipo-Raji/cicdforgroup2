7410309d9cdb0a898cf48d0a9251c3fad62503a9
joyalaji <joyalmampillil4@gmail.com>
2026-03-17 16:38:30 -0400

    Fix typo in welcome message check

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -27,8 +27,8 @@ if ! grep -q "March 17, 2026" myapp/index.html; then
   exit 1
 fi
 
-if ! grep -q "Wellcome to my app" myapp/index.html; then
-   echo "ERROR: WELlCOME QOUTE IS MISSING." >&2
+if ! grep -q "Welcome to my app" myapp/index.html; then
+   echo "ERROR: WELCOME QOUTE IS MISSING." >&2
    exit 1 
 fi
 
