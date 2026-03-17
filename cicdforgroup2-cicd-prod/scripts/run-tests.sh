64d7950cfae81c6b84b4dc57ded4d612b825cc3d
Banky4567 <yisabankole@gmail.com>
2026-03-17 17:14:00 -0400

    Update date check in run-tests.sh script

--- cicdforgroup2-cicd-prod/scripts/run-tests.sh
+++ cicdforgroup2-cicd-prod/scripts/run-tests.sh
@@ -22,18 +22,13 @@ if [[ ! -f "myapp/script.js" ]]; then
   exit 1
 fi
 
-fi -f "myapp/manifest.json" || 
-  echo "ERROR: myapp/manifest.json not found." >&2
-  exit 1
-fi
-
 fi -f "myapp/service-worker.js" || 
   echo "ERROR: myapp/service-worker.js not found." >&2
   exit 1
 fi
 
-if ! grep -q "March 17, 2026" myapp/index.html; then
-  echo "ERROR: Today's date (March 17, 2026) not found in index.html." >&2
+if ! grep -q "My Birthday is Dec 23, 2026" myapp/index.html; then
+  echo "ERROR: My Birthday date (Dec 23, 2026) not found in index.html." >&2
   exit 1
 fi
 
